3a0f7715631de80ae50fcbe4825614dac596c473
wangpy <mr_wpy@163.com>
2021-11-19 14:18:30 +0800

    优化打包体积

--- examples/main.ts
+++ examples/main.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+
 import '../src/assets/style/common.css'
 import '../src/assets/style/group.css'
 
